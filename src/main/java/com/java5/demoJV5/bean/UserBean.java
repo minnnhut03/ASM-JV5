79e7ecca769e34228192eacdfab928e6c9988db0
mnhut03 <dangminhnhut2005vl@gmail.com>
2025-02-17 15:06:32 +0700

    updatebean

--- src/main/java/com/java5/demoJV5/bean/UserBean.java
+++ src/main/java/com/java5/demoJV5/bean/UserBean.java
@@ -1,5 +1,6 @@
 package com.java5.demoJV5.bean;
 
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,8 +11,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginBean {
-    
+public class UserBean {
+    private int userId;
+
     @NotBlank(message = "Email không được để trống")
     @Email(message = "Email không hợp lệ")
     @Size(max = 255, message = "Email không được vượt quá 255 ký tự")
@@ -20,4 +22,16 @@ public class LoginBean {
     @NotBlank(message = "Mật khẩu không được để trống")
     @Size(min = 6, max = 20, message = "Mật khẩu phải từ 6 đến 20 ký tự")
     private String password;
+
+    @NotBlank(message = "Họ và tên không được để trống")
+    @Size(max = 100, message = "Họ và tên không được vượt quá 100 ký tự")
+    private String fullName;
+
+    @NotBlank(message = "Địa chỉ không được để trống")
+    @Size(max = 255, message = "Địa chỉ không được vượt quá 255 ký tự")
+    private String address;
+    
+    private Integer role = 1; 
+
+    private Boolean status = true;
 }
